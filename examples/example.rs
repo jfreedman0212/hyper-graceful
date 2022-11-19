b5641e153083bd5a206117dec11d535960fa5bea
Josh Freedman <joshfreedman@pm.me>
2022-11-18 21:46:10 -0500

    Upgrade to Hyper v1 release candidate

--- examples/example.rs
+++ examples/example.rs
@@ -1,7 +1,8 @@
-use futures::stream::{self, StreamExt};
-use hyper::server::conn::Http;
+use http_body_util::Full;
+use hyper::body::{self, Bytes};
+use hyper::server::conn::http1;
 use hyper::service::service_fn;
-use hyper::{Body, Response};
+use hyper::{Request, Response};
 use hyper_graceful::ConnectionManager;
 use std::convert::Infallible;
 use std::net::SocketAddr;
@@ -17,23 +18,17 @@ async fn main() {
     let address = SocketAddr::from(([127, 0, 0, 1], 3000));
     let listener = TcpListener::bind(&address).await.unwrap();
     println!("Listening on: {}", listener.local_addr().unwrap());
-    let http = Http::new();
     let manager = ConnectionManager::default();
 
-    let svc = service_fn(|_| async {
-        let s = stream::iter(0..10).then(|i| async move {
-            tokio::time::sleep(Duration::from_secs(1)).await;
-            Ok::<_, Infallible>(format!("{}\n", 10 - i))
-        });
-        Ok::<_, Infallible>(Response::new(Body::wrap_stream(s)))
-    });
+    let svc = service_fn(wait_ten_seconds);
 
     loop {
         tokio::select! {
             conn = listener.accept() => {
                 if let Ok((tcp_stream, socket)) = conn {
                     println!("Receiving connection from: {}", socket);
-                    let future = manager.manage_connection(http.serve_connection(tcp_stream, svc), move |conn| {
+                    let conn = http1::Builder::new().serve_connection(tcp_stream, svc);
+                    let future = manager.manage_connection(conn, move |conn| {
                         println!("Gracefully shutting down connection: {}", socket);
                         conn.graceful_shutdown();
                     });
@@ -41,11 +36,21 @@ async fn main() {
                 }
             },
             _ = ctrl_c() => {
+                println!("Received Ctrl-C signal, attempting graceful shutdown");
                 break;
             }
         }
     }
-    let (graceful, forced) = manager.graceful_shutdown(Duration::from_secs(5)).await;
+    let (graceful, forced) = manager.graceful_shutdown(Duration::from_secs(15)).await;
     println!("Gracefully shutdown {} connections", graceful);
     println!("Forcefully shutdown {} connections", forced);
+}
+
+async fn wait_ten_seconds(
+    _req: Request<body::Incoming>,
+) -> Result<Response<Full<Bytes>>, Infallible> {
+    tokio::time::sleep(Duration::from_secs(10)).await;
+    Ok(Response::new(Full::new(Bytes::from(
+        "I waited 10 seconds for this?",
+    ))))
 }
